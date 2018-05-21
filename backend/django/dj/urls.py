058e2c14314653b97c57a3aa2d63401caf12b179
Alin Cijov <rcijov>
2018-05-21 01:19:25 -0400

    Changed the structure to add django and flask into the backend

--- backend/django/dj/urls.py
+++ backend/django/dj/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^vote/', views.post, name='vote')
+    url(r'^postjson/', views.post, name='vote')
 ]
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd backend && docker build -t flask . && docker run -d -p 9999:9999 -t flask
+cd backend/flask && docker build -t flask . && docker run -d -p 9999:9999 -t flask
 cd ..
 cd frontend && docker build -t angular . && docker run -d -p 8888:80 -t angular
