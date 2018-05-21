1a54d9768999e9a520896b419309f9c36ea45f34
Alin Cijov <rcijov>
2018-05-21 03:46:03 -0400

    Added CORS for the django + deploy case to switch between flask/django

--- backend/django/dj/settings.py
+++ backend/django/dj/settings.py
@@ -36,18 +36,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'dj.urls'
 
 TEMPLATES = [
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-cd backend/flask && docker build -t flask . && docker run -d -p 9999:9999 -t flask
-cd ..
+
+read -p  "Which backend to install [flask,django] : " doit 
+case $doit in  
+	  flask) cd backend/flask && docker build -t flask . && docker run -d -p 9999:9999 -t flask ;; 
+  	  django) cd backend/django && docker build -t django . && docker run -d -p 9999:9999 -t django ;; 
+	  *) echo Unknown command ;; 
+esac
+
+cd ../..
 cd frontend && docker build -t angular . && docker run -d -p 8888:80 -t angular
